faffdb21504dbe01258fa2cd6237292917f5a4df
javadeveloper110 <javadeveloper110@gmail.com>
2017-02-06 23:14:03 +0300

    create

---
+++ clear.bat
@@ -0,0 +1,3 @@
+del src\com\example\testapp\R.java
+del *.keystore
+del /s /f /q bin\*.*
---
+++ src/com/example/testapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button button = new Button(this);
+        button.setText("Big button");
+        
+        setContentView(button);
+    }
+}
