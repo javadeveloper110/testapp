1aec267e316d8a46b86d70a4b986b5c59a30448f
javadeveloper110 <javadeveloper110@gmail.com>
2017-02-13 16:42:41 +0300

    info by program, shaders

--- src/com/example/testapp/MyRenderer.java
+++ src/com/example/testapp/MyRenderer.java
@@ -5,6 +5,10 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLES20;
 import android.util.Log;
+import java.util.Map;
+import java.util.Hashtable;
+import java.util.Map.Entry;
+import java.util.Date;
 
 public class MyRenderer implements Renderer
 {
@@ -68,13 +72,15 @@ public class MyRenderer implements Renderer
             throw new Exception(GLES20.glGetShaderInfoLog(shader));
         
         //Log.i(TAG, "MyRenderer.loadShader end");
+        __logShaderInfo(shader);
         
         return shader;
     }
     
     public void onSurfaceCreated(GL10 unused, EGLConfig config)
     {
-        Log.v(TAG, "\n\n\n=============================================================================");
+        Date d = new Date();
+        Log.v(TAG, "\n\n"+ 	d.toLocaleString() +"\n=============================================================================");
         //Log.i(TAG, "MyRenderer.onSurfaceCreated begin");
         
         try
@@ -106,6 +112,8 @@ public class MyRenderer implements Renderer
                 throw new Exception(GLES20.glGetProgramInfoLog(program));
             
             GLES20.glUseProgram(program);
+            
+            __logProgramInfo(program);
         }
         catch(Exception e)
         {
@@ -115,5 +123,57 @@ public class MyRenderer implements Renderer
         GLES20.glClearColor((float)Math.random(), (float)Math.random(), (float)Math.random(), 1.0f);
         
         //Log.i(TAG, "MyRenderer.onSurfaceCreated end");
+    }
+    
+    private void __logProgramInfo(int program)
+    {
+        Log.i(TAG, "MyRenderer.__logProgramInfo (program: "+ program +"):");
+        
+        int params[] = new int[1];
+        Map<Integer, String> map = new Hashtable<Integer, String>();
+        
+        map.put(GLES20.GL_ACTIVE_ATTRIBUTES, "GL_ACTIVE_ATTRIBUTES");
+        map.put(GLES20.GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, "GL_ACTIVE_ATTRIBUTE_MAX_LENGTH");
+        map.put(GLES20.GL_ACTIVE_UNIFORMS, "GL_ACTIVE_UNIFORMS");
+        map.put(GLES20.GL_ACTIVE_UNIFORM_MAX_LENGTH, "GL_ACTIVE_UNIFORM_MAX_LENGTH");
+        map.put(GLES20.GL_ATTACHED_SHADERS, "GL_ATTACHED_SHADERS");
+        map.put(GLES20.GL_DELETE_STATUS, "GL_DELETE_STATUS");
+        map.put(GLES20.GL_INFO_LOG_LENGTH, "GL_INFO_LOG_LENGTH");
+        map.put(GLES20.GL_LINK_STATUS, "GL_LINK_STATUS");
+        map.put(GLES20.GL_VALIDATE_STATUS, "GL_VALIDATE_STATUS");
+        
+        for (Map.Entry<Integer, String> entry : map.entrySet())
+        {
+            GLES20.glGetProgramiv(program, entry.getKey(), params, 0);
+            Log.i(TAG, "program["+ entry.getValue() +"]: "+ params[0]);
+        }
+        
+        Log.i(TAG, "program info log: "+ GLES20.glGetProgramInfoLog(program));
+        
+        Log.i(TAG, "\n\n\n");
+    }
+    
+    private void __logShaderInfo(int shader)
+    {
+        Log.i(TAG, "MyRenderer.__logShaderInfo (shader: "+ shader +"):");
+        
+        int params[] = new int[1];
+        Map<Integer, String> map = new Hashtable<Integer, String>();
+        
+        map.put(GLES20.GL_COMPILE_STATUS, "GL_COMPILE_STATUS");
+        map.put(GLES20.GL_DELETE_STATUS, "GL_DELETE_STATUS");
+        map.put(GLES20.GL_INFO_LOG_LENGTH, "GL_INFO_LOG_LENGTH");
+        map.put(GLES20.GL_SHADER_SOURCE_LENGTH, "GL_SHADER_SOURCE_LENGTH");
+        map.put(GLES20.GL_SHADER_TYPE, "GL_SHADER_TYPE");
+        
+        for (Map.Entry<Integer, String> entry : map.entrySet())
+        {
+            GLES20.glGetShaderiv(shader, entry.getKey(), params, 0);
+            Log.i(TAG, "shader["+ entry.getValue() +"]: "+ params[0]);
+        }
+        
+        Log.i(TAG, "shader info log: "+ GLES20.glGetShaderInfoLog(shader));
+        
+        Log.i(TAG, "\n\n\n");
     }
 }
